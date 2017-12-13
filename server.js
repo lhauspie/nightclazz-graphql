2d85aefb3e28409ea275e88245fe1016bca82b85
Logan HAUSPIE <logan.hauspie.pro@gmail.com>
2017-12-13 21:31:07 +0100

    be able to search nearest antennas of the found address

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var request = require('request');
 var requestPromise = require('request-promise');
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+var {buildSchema} = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -16,10 +16,23 @@ var schema = buildSchema(`
     latitude: Float
   }
   
+  type NearestAntenna {
+    coordinates: GPS
+    generation: String
+    provider: String
+    lastUpdate: String
+    status: String
+    dist: Int
+    addressLabel: String
+    city: String
+    insee: String
+  }
+
   type City {
     coordinates: GPS
     zipCode: String
     city: String
+    nearestAntenna(generation: String, provider: String): NearestAntenna
   }
 `);
 
@@ -29,50 +42,99 @@ class GPS {
         this.latitude
     }
 }
+
 class City {
     constructor() {
         this.coordinates
         this.zipCode
         this.city
     }
+
+    nearestAntenna({generation, provider}) {
+        var options = {
+            uri: `https://data.anfr.fr/api/records/1.0/search/?dataset=observatoire_2g_3g_4g&geofilter.distance=${this.coordinates.latitude}%2C${this.coordinates.longitude}%2C100&refine.adm_lb_nom=${provider}`,
+            json: true
+        };
+
+        return requestPromise(options)
+            .then(function (json) {
+                console.log("###############")
+                console.log(JSON.stringify(json))
+//                console.log(JSON.stringify(json.parameters))
+                return json;
+                //console.log(json.features[0].geometry)
+                //console.log(json.features[0].properties)
+                //console.log(json.features[1].geometry)
+                //console.log(json.features[1].properties)
+            })
+            .then(function (json) {
+                var gps = new GPS();
+                gps.longitude = json.records[0].fields.coordonnees[0]
+                gps.latitude = json.records[0].fields.coordonnees[1]
+                var nearestAntenna = new NearestAntenna()
+                nearestAntenna.coordinates = gps
+                nearestAntenna.generation = json.records[0].fields.generation
+                nearestAntenna.provider = json.records[0].fields.adm_lb_nom
+                nearestAntenna.lastUpdate = json.records[0].fields.date_maj
+                nearestAntenna.status = json.records[0].fields.en_service
+                nearestAntenna.dist = json.records[0].fields.dist
+                nearestAntenna.addressLabel = json.records[0].fields.adr_lb_add1
+                nearestAntenna.city = json.records[0].fields.nom_com
+                nearestAntenna.insee = json.records[0].fields.code_insee
+
+                return nearestAntenna
+            })
+    }
+}
+
+class NearestAntenna {
+    constructor() {
+        this.coordinates
+        this.generation
+        this.provider
+        this.lastUpdate
+        this.status
+        this.dist
+        this.addressLabel
+        this.city
+        this.insee
+    }
 }
 
 // The root provides a resolver function for each API endpoint
 var root = {
-    hello: () => {
-        return 'Hello world!';
-    },
     search: searchCity,
 };
 
 function searchCity({query}) {
     console.log(query)
-  var options = {
-    uri: `https://api-adresse.data.gouv.fr/search/?q=${query}`,
-    json: true
-  };
+    var options = {
+        uri: `https://api-adresse.data.gouv.fr/search/?q=${query}`,
+        json: true
+    };
 
-  return requestPromise(options)
-    .then(function(json) {
-        console.log(JSON.stringify(json))
-        console.log(JSON.stringify(json.query))
-        return json;
-        //console.log(json.features[0].geometry)
-        //console.log(json.features[0].properties)
-        //console.log(json.features[1].geometry)
-        //console.log(json.features[1].properties)
-    })
-    .then(function(json) {
-        var gps = new GPS();
-        gps.longitude = json.features[0].geometry.coordinates[0];
-        gps.latitude = json.features[0].geometry.coordinates[1];
+    return requestPromise(options)
+        .then(function (json) {
+            console.log(JSON.stringify(json))
+            console.log(JSON.stringify(json.query))
+            return json;
+            //console.log(json.features[0].geometry)
+            //console.log(json.features[0].properties)
+            //console.log(json.features[1].geometry)
+            //console.log(json.features[1].properties)
+        })
+        .then(function (json) {
+            var gps = new GPS();
+            gps.longitude = json.features[0].geometry.coordinates[0];
+            gps.latitude = json.features[0].geometry.coordinates[1];
 
-        var city = new City();
-        city.zipCode = json.features[0].citycode;
-        city.coordinates = gps;
+            var city = new City();
+            city.zipCode = json.features[0].properties.citycode;
+            city.coordinates = gps;
+            city.city = json.features[0].properties.city;
 
-        return city;
-    })
+            return city;
+        })
 }
 
 var app = express();
